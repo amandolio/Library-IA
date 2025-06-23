3d04fd20ee5575c20b602c1c3f45e5e5ded8a8bd
amandolio <alainraidel9@gmail.com>
2025-06-23 11:06:37 -0400

    Updated TopicSearch.tsx

--- src/components/TopicSearch.tsx
+++ src/components/TopicSearch.tsx
@@ -208,7 +208,7 @@ export function TopicSearch() {
               value={topic}
               onChange={(e) => setTopic(e.target.value)}
               onKeyPress={(e) => e.key === 'Enter' && handleSearch(topic)}
-              placeholder="Ej: Machine Learning, Quantum Computing, Cybersecurity..."
+              placeholder="  Ej: Machine Learning, Quantum Computing, Cybersecurity..."
               className="w-13 pl-4 pr-32 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent text-lg"
             />
             <button
