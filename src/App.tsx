e726fe742011f981778418ae35b546f7eb21284c
amandolio <alainraidel9@gmail.com>
2026-07-10 14:44:21 -0400

    Fix: Import useEffect in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { Dashboard } from './components/Dashboard';
