a61d29ebcd7583a4cb36f6488fa67fdbe7d86664
amandolio <alainraidel9@gmail.com>
2026-07-17 16:02:23 -0400

    Add new resources and update registry

--- src/data/resourceRegistry.ts
+++ src/data/resourceRegistry.ts
@@ -6,6 +6,8 @@ import { additionalResources4 } from './additionalResources4';
 import { additionalResources5 } from './additionalResources5';
 import { additionalResources6 } from './additionalResources6';
 import { additionalResources7 } from './additionalResources7';
+import { additionalResources8 } from './additionalResources8';
+import { additionalResources9 } from './additionalResources9';
 
 export const allAdditionalResources: Resource[] = [
   ...additionalResources,
@@ -15,4 +17,6 @@ export const allAdditionalResources: Resource[] = [
   ...additionalResources5,
   ...additionalResources6,
   ...additionalResources7,
+  ...additionalResources8,
+  ...additionalResources9,
 ];
